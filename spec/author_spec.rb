a966342db41c0294f571134233838bc5ac90f5ea
Celina Jackson <celinanjackson@gmail.com>
2020-07-15 00:08:26 +0000

    Automatically backed up by Learn

--- spec/author_spec.rb
+++ spec/author_spec.rb
@@ -16,7 +16,7 @@ describe "Author" do
       end
     end
 
-    
+
 
     describe "#add_post" do
       it "takes in an argument of a post and associates that post with the author by telling the post that it belongs to that author" do
